cd38d282c2d341a67399a7e5754e3676be2a14e1
Tiziano Forte <forte.tiziano@gmail.com>
2022-07-12 10:32:19 +0200

    2th components exercise

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import  ReactDOM  from 'react-dom'
+import { App } from './app'
+import React from 'react';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
