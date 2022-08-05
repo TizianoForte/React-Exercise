23f94213e4683f5e2d24d91c4f839a3a9a0adf66
Tiziano Forte <forte.tiziano@gmail.com>
2022-08-05 16:42:42 +0200

    form-05 exercise

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Login } from "./Login";
+import { UncontrolledLogin } from "./UncontrolledLogin";
 
 
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
     return (
       <div>
         <Login/>
+        <UncontrolledLogin/>
       </div>
     );
   }
