072c90085c2116570458e95caa1dcf6cd2f9a570
Tiziano Forte <forte.tiziano@gmail.com>
2022-08-17 16:17:09 +0200

    list-04 exercise

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { TodoList } from "./todolist";
+import TodoList from "./todolist";
 
 
 
--- src/todolist.js
+++ src/todolist.js
@@ -2,42 +2,41 @@ import { Component } from "react";
 import mylist from "./mylist";
 
 
-export class TodoList extends Component{
+export  class TodoList extends Component {
     state = {
-        inputTodo: "",
-        data: mylist,
-        
-      };
-    
-      saveTodo = (event) => {
-        const todo = event.target.value;
-        this.setState({
-          inputTodo: todo,
-        });
-      };
-    
-      addTodo = () => {
-        this.setState({
-          data: [...mylist, { todo: this.state.inputTodo }],
-        });
-      };
-    
-      render() {
-        return (
-          <>
-            <h1>Todo List</h1>
-            <input type="text" onChange={this.saveTodo} />
-            <button onClick={this.addTodo}>Add</button>
-            <ul>
-              {this.state.data.map((item, id) => {
-                return <li key={id}>{item.todo}</li>;
-              })}
-            </ul>
-          </>
-        );
-      }
+      inputTodo: "",
+      data: mylist,
+    };
+  
+    saveTodo = (event) => {
+      const todo = event.target.value;
+      this.setState({
+        inputTodo: todo,
+      });
+    };
+  
+    addTodo = () => {
+      this.setState({
+        data: [...mylist, { todo: this.state.inputTodo }],
+        inputTodo: ""
+      });
+    };
+  
+    render() {
+      return (
+        <>
+          <h1>Todo List</h1>
+          <input type="text" onChange={this.saveTodo} value={this.state.inputTodo}/>
+          <button onClick={this.addTodo}>Add</button>
+          <ul>
+            {this.state.data.map((item, key) => {
+              return <li key={key}>{item.todo}</li>;
+            })}
+          </ul>
+        </>
+      );
     }
-    export default TodoList
+  };
 
 
-    //mi fa aggiungere solo un elemento però non capisco il motivo
+  export default TodoList
