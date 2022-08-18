d0ce9837ae407c22684bda519247d0b9718c57e2
Tiziano Forte <forte.tiziano@gmail.com>
2022-08-18 16:47:35 +0200

    list-05

--- src/todolist.js
+++ src/todolist.js
@@ -1,37 +1,46 @@
+
 import { Component } from "react";
-import mylist from "./mylist";
+
 
 
 export  class TodoList extends Component {
     state = {
-      inputTodo: "",
-      data: mylist,
+      newTodo: "",
+      data: [],
     };
   
     saveTodo = (event) => {
       const todo = event.target.value;
       this.setState({
-        inputTodo: todo,
+        newTodo: todo,
       });
     };
   
     addTodo = () => {
       this.setState({
-        data: [...mylist, { todo: this.state.inputTodo }],
-        inputTodo: ""
+        data: [...this.state.data, { todo: this.state.newTodo }],
+        newTodo: ""
       });
     };
-  
+
+    removeTodo = () => {
+      this.setState({
+        data: [],
+      });
+    }
+    
     render() {
       return (
         <>
           <h1>Todo List</h1>
-          <input type="text" onChange={this.saveTodo} value={this.state.inputTodo}/>
+          <input type="text" onChange={this.saveTodo} value={this.state.newTodo}/>
           <button onClick={this.addTodo}>Add</button>
+          <button onClick={this.removeTodo}>Reset All</button>
           <ul>
             {this.state.data.map((item, key) => {
               return <li key={key}>{item.todo}</li>;
             })}
+            
           </ul>
         </>
       );
