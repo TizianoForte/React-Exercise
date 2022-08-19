0d189ceea8f033684b838edfad1a0c2b4959356d
Tiziano Forte <forte.tiziano@gmail.com>
2022-08-19 15:00:51 +0200

    funcion-component-01

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import React from "react";
-import { DisplayLanguage } from "./DisplayLanguage";
-import { languageContext } from "./LanguageContext";
+import Welcome from "./Welcome";
 
-class App extends React.Component {
-  state = {
-		language: "en",
-	};
 
-	handleLenguageChange = (event) => {
-		this.setState({
-			language: event.target.value,
-		});
-		console.log(this.state);
-	};
-
-	onLogin = (state) => console.log(state);
-
-	render() {
-		return (
-			<>
-				<languageContext.Provider value={this.state.language}>
-					<DisplayLanguage onChange={this.handleLenguageChange}/>
-				</languageContext.Provider>
-			</>
-		);
-	}
+export default class App extends React.Component {
+  render() {
+    return <Welcome name="Tiziano"/>;
+  }
 }
-
-
-export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export  function Welcome(props) {
+  return (
+    <div>
+        <h1>Welcome {props.name}</h1>
+    </div>
+  )
+}
+
+export default Welcome
