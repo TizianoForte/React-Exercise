c415f28fc6684410b2b876a0d0655051d1f8446d
Tiziano Forte <forte.tiziano@gmail.com>
2022-08-26 15:41:46 +0200

    React Router-04

--- src/App.js
+++ src/App.js
@@ -3,18 +3,42 @@ import { Route, Routes } from "react-router-dom";
 import Welcome from "./Welcome";
 import Counter from "./Counter";
 import ShowGithubUser from "./ShowGithubUser";
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
 
 
 export default function App() {
+  const navigate = useNavigate();
+
+  const [usernameValue, setUsernameValue] = useState();
+
+  const handleNavigate = () => {
+    navigate(`/users/${usernameValue}`);
+  };
+
   return (
     <div>
+      <ul>
+        <li>
+          <Link to={"/"}>WELCOME</Link>
+        </li>
+        <li>
+          <Link to={"/counter"}>COUNTER</Link>
+        </li>
+        <li>
+          <input
+            type="text"
+            onKeyUp={(e) => setUsernameValue(e.target.value)}
+          />
+          <button onClick={handleNavigate}>USERNAME</button>
+        </li>
+      </ul>
       <Routes>
         <Route path="/" element={<Welcome name="Tiziano" />} />
-        <Route path="/counter" element={<Counter/>} />
-        <Route path="user/:username" element={<ShowGithubUser />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="users/:username" element={<ShowGithubUser />} />
       </Routes>
     </div>
   );
 }
-
-
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,7 @@
 
 import React from "react"
 
-export default function Welcome({name = "World"}) {
+export default function Welcome({name='World'}) {
   return (
     <>
         <h1>Welcome, {name}!</h1>
