0077f25bf0513e2054b192d745eb6848ec41db56
Tiziano Forte <forte.tiziano@gmail.com>
2022-08-29 15:33:23 +0200

    SWR*03

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,17 +2,18 @@ import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser({ username }) {
     
-    const {users, error, isLoading} = useGithubUser(username);
+    const {users, onFetchUser} = useGithubUser(username);
 
+    function handleFetch(){
+        onFetchUser()
+    }
     
     return(
         <div>
             {users && <h1>{users.name}</h1>}
             {users && <img alt="avatar_prof" src={users.avatar_url}></img>}
             {users && <h3>{users.location}</h3>}
-            {users && <h5>{users.bio}</h5>}
-            {isLoading && <h1>Loading...</h1>}
-            {error && <h1>Error!! User not found</h1>}
+            <button onClick={handleFetch}>Reaload data</button>
         </div>
     )
 }
